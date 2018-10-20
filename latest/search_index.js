fc962194510e3a13c40693c56822d0f38f5bc4d7
autodocs <autodocs>
2018-10-20 14:56:46 +0000

    build based on 8c1b1ae

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,15 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "LightQuery.@_",
     "category": "macro",
-    "text": "macro _(body::Expr)\n\nAnother syntax for anonymous functions. The arguments are inside the body; the first arguments is _, the second argument is __, etc.\n\njulia> using LightQuery\n\njulia> 1 |> (@_ _ + 1)\n2\n\njulia> map((@_ __ - _), (1, 2), (2, 1))\n(1, -1)\n\n\n\n\n\n"
-},
-
-{
-    "location": "index.html#LightQuery.@query-Tuple{Any}",
-    "page": "Home",
-    "title": "LightQuery.@query",
-    "category": "macro",
-    "text": "macro query(body::Expr)\n\nQuery your code. If body is a chain head_ |> tail_, recur on head. If tail is a function call, and the function ends with a number (the parity), anonymize and quote arguments past that parity. Either way, anonymize the whole tail, then call it on head.\n\njulia> using LightQuery\n\njulia> call(source1, source2, (anonymous, quoted)) =\n            anonymous(source1, source2), quoted;\n\njulia> @query 1 |> (_ - 2) |> abs(_) |> call2(_, 2, _ + __)\n(3, :(_ + __))\n\njulia> @query 1 |> call2(_)\nERROR: LoadError: Expecting at least 2 argument(s)\n[...]\n\njulia> function call_keywords(source1; anonymous_quoted)\n            anonymous, quoted = anonymous_quoted\n            anonymous(source1), quoted\n        end;\n\njulia> @query 1 |> call_keywords1(_, anonymous_quoted = _ + 1)\n(2, :(_ + 1))\n\njulia> @query 1 |> call_keywords1(_; anonymous_quoted = _ + 1)\n(2, :(_ + 1))\n\n\n\n\n\n"
+    "text": "macro _(body::Expr)\n\nCreate an Anonymouser object. The arguments are inside the body; the first arguments is _, the second argument is __, etc. Handly stores a quoted version of the function.\n\njulia> using LightQuery\n\njulia> 1 |> @_(_ + 1)\n2\n\njulia> map(@_(__ - _), (1, 2), (2, 1))\n(1, -1)\n\njulia> @_(_ + 1).expression\n:(_ + 1)\n\n\n\n\n\n"
 },
 
 {
