4af86bbc8d7169b98ecb40de4db83a6aeefea3f4
autodocs <autodocs>
2018-10-21 01:07:19 +0000

    build based on dee9554

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,11 +9,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#LightQuery.Nameless",
+    "page": "Home",
+    "title": "LightQuery.Nameless",
+    "category": "type",
+    "text": "A container for a function and the expression that generated it\n\n\n\n\n\n"
+},
+
+{
     "location": "index.html#LightQuery.@_-Tuple{Expr}",
     "page": "Home",
     "title": "LightQuery.@_",
     "category": "macro",
-    "text": "macro _(body::Expr)\n\nCreate an Anonymouser object. The arguments are inside the body; the first arguments is _, the second argument is __, etc. Handly stores a quoted version of the function.\n\njulia> using LightQuery\n\njulia> 1 |> @_(_ + 1)\n2\n\njulia> map(@_(__ - _), (1, 2), (2, 1))\n(1, -1)\n\njulia> @_(_ + 1).expression\n:(_ + 1)\n\n\n\n\n\n"
+    "text": "macro _(body::Expr)\n\nCreate an Nameless object. The arguments are inside the body; the first arguments is _, the second argument is __, etc. Also stores a quoted version of the function.\n\njulia> using LightQuery\n\njulia> 1 |> @_(_ + 1)\n2\n\njulia> map(@_(__ - _), (1, 2), (2, 1))\n(1, -1)\n\njulia> @_(_ + 1).expression\n:(_ + 1)\n\n\n\n\n\n"
 },
 
 {
