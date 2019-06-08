38f1c3580d81ef2d0c381c7f73a6e50ccfc45ce5
bramtayl <brandon.taylor221@gmail.com>
2019-06-08 11:12:53 -0400

    undo

--- test/submit_coverage.jl
+++
@@ -1,7 +0,0 @@
-if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" &&
-    get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
-    using Pkg: instantiate
-    using Coverage.Codecov: submit, process_folder
-    instantiate()
-    submit(process_folder())';
-end
--- test/travis.jl
+++
@@ -1,6 +0,0 @@
-if VERSION >= "1.1.0-rc1"
-    Pkg.build(verbose=true)
-else
-    Pkg.build()
-    Pkg.test(coverage = true)
-end
