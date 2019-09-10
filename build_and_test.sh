a02c670a39ccd796ca6a8a8dba37eadfd13448fe
Brandon Taylor <brandon.taylor221@gmail.com>
2019-09-10 07:22:42 -0400

    fix

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,11 +1,8 @@
 if [[ -a .git/shallow ]]
-    then
-        git fetch --unshallow
+    then git fetch --unshallow
 fi
 
-if [[ $TRAVIS_JULIA_VERSION == 1.0]]
-    then
-        julia --project --code-coverage=tracefile-%p.info --code-coverage=user build_and_test.jl
-    else
-        julia --project --code-coverage=user build_and_test.jl
+if [[ $TRAVIS_JULIA_VERSION == 1.0 ]]
+    then julia --project --code-coverage=tracefile-%p.info --code-coverage=user build_and_test.jl
+    else julia --project --code-coverage=user build_and_test.jl
 fi
