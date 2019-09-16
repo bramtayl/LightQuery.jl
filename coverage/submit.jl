ddc2ccabdbb888c9ebe7ceb4ee84844532398242
Brandon Taylor <brandon.taylor221@gmail.com>
2019-09-16 17:03:48 -0400

    fix

--- coverage/submit.jl
+++ coverage/submit.jl
@@ -4,4 +4,22 @@ instantiate()
 using Coverage: process_folder
 using Coverage.Codecov: submit
 
-submit(process_folder())
+using Pkg: instantiate
+instantiate()
+
+using Coverage: process_folder
+using Coverage.Codecov: submit
+using Coverage.LCOV: readfolder
+
+submit(
+    @static if VERSION >= v"1.0"
+        filter(
+            let prefix = joinpath(pwd(), "src", "")
+                coverage -> startswith(coverage.filename, prefix)
+            end,
+            readfolder(".")
+        )
+    else
+        process_folder()
+    end
+)
