0b6ae764e46b5cc070d8d49dfa1340330079419b
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-07 12:16:19 -0500

    fix

--- coverage/submit.jl
+++ coverage/submit.jl
@@ -1,5 +1,7 @@
-using Pkg: instantiate
-instantiate()
-using Coverage: process_folder
-using Coverage.Codecov: submit
-submit(process_folder(pwd()))
+if VERSION > v"1.3"
+    using Pkg: instantiate
+    instantiate()
+    using Coverage: process_folder
+    using Coverage.Codecov: submit
+    submit(process_folder(pwd()))
+end
