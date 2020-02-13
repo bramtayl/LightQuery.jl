b3dc4197d9866a203d9ff2a1124c270f73f1eb4b
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-13 12:04:13 -0500

    fix

--- coverage/submit.jl
+++ coverage/submit.jl
@@ -1,13 +1,11 @@
-@static if VERSION >= v"1.1"
-    using Pkg: instantiate
-    instantiate()
-    using Coverage
-    coverage = process_folder()
-    coverage = merge_coverage_counts(coverage, filter!(
-        let prefix = joinpath(pwd(), "src", "")
-            coverage_file -> startswith(coverage_file.filename, prefix)
-        end,
-        LCOV.readfolder(pwd())
-    ))
-    Codecov.submit(coverage)
-end
+using Pkg: instantiate
+instantiate()
+using Coverage
+coverage = process_folder()
+coverage = merge_coverage_counts(coverage, filter!(
+    let prefix = joinpath(pwd(), "src", "")
+        coverage_file -> startswith(coverage_file.filename, prefix)
+    end,
+    LCOV.readfolder(pwd())
+))
+Codecov.submit(coverage)
