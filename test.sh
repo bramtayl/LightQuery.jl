5658d83433c76bc4fe029c452da64f600e9bb4ec
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-13 10:37:50 -0500

    fix

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 if test TRAVIS_JULIA_VERSION = "1.0"
     then julia --project --code-coverage=user test.jl
-    else julia --project --code-coverage=user --code-coverage=tracefile-%p.info test.jl
+else julia --project --code-coverage=user --code-coverage="tracefile-%p.info" test.jl
 fi
