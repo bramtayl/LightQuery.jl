0297ffee359a3923abdff226944e7d6d494d7dbb
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-22 19:50:28 -0500

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,3 @@
-using Pkg: develop, instantiate, PackageSpec
-develop(PackageSpec(path=pwd()))
-instantiate()
-
 using LightQuery
 using Documenter: deploydocs, makedocs
 makedocs(sitename = "LightQuery.jl", modules = [LightQuery], doctest = false)
