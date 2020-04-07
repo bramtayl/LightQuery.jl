820af6c36f7f4dd2c7d904038b5267edbd163eea
Brandon Taylor <brandon.taylor221@gmail.com>
2020-04-07 11:26:17 -0400

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using LightQuery: By, mix, @name
 using Test: @test_throws
 
 # TODO: figure out a better way to trigger this
-@test_throws StackOverflowError iterate((@name mix(:inner,
-    By(Int[], identity),
-    By(Int[], identity)
-)), (nothing, nothing, false, false))
+@test_throws StackOverflowError iterate(
+    (@name mix(:inner, By(Int[], identity), By(Int[], identity))),
+    (nothing, nothing, false, false),
+)
